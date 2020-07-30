6a11a2647d896811cfa10b9dbb158ac2dca6b2c2
SoyMarco <marco_asr@outlook.com>
2020-07-29 22:18:22 -0500

    verPerfil

---
+++ routers/procesoToken.go
@@ -0,0 +1,44 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/SoyMarco/twittor/bd"
+	"github.com/SoyMarco/twittor/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+/*Email valor usaso en los EndPoints*/
+var Email string
+
+/*IDUsuario es el ID devuelto del modelo, que se usará en todos los EndPoints*/
+var IDUsuario string
+
+/*ProcesoToken es para extraer sus valores */
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+	miClave := []byte("Ohana987654321")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token Inválido")
+	}
+	return claims, false, string(""), err
+}
